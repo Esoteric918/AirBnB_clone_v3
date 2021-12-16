e3e98f1813ac881202518356a51aa5449b187544
Esoteric918 <3411@holbertonschool.com>
2021-12-16 11:51:17 -0600

    task 5 add stats to index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,18 @@
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
 
 
 @app_views.route('/status', strick_slashes=False)
 def status():
     return jsonify(status="OK")
+
+@app_views.route('/api/v1/stats')
+def stats():
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   user=storage.count("User"))
