2b9f2812a06cbcee5b01bc9c560c6ee5808a947c
Hope-Parnell <parnell.hope.c@gmail.com>
2021-12-16 11:53:17 -0600

    fixed typos added blueprint

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 from os import getenv
 # Start Flask app
 app = Flask(__name__)
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,11 +5,11 @@ from flask import Flask, jsonify
 from models import storage
 
 
-@app_views.route('/status', strick_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify(status="OK")
 
-@app_views.route('/api/v1/stats')
+@app_views.route('/api/v1/stats', strict_slashes=False)
 def stats():
     return jsonify(amenities=storage.count("Amenity"),
                    cities=storage.count("City"),
