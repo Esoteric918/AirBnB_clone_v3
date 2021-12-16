e8cf68fdd18dc790fa192b3ba856ed8df7177245
Esoteric918 <3411@holbertonschool.com>
2021-12-16 11:14:56 -0600

    Blueprint set up

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+from api.v1.views import app_views
+from flask import Flask, jsonify
+
+
+@app_views.route('/status', strick_slashes=False)
+def status():
+    return jsonify(status="OK")
