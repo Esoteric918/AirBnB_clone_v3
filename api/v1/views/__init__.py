637a57c5de562ef4aac65b0a39ea5c5d30471f10
Hope-Parnell <parnell.hope.c@gmail.com>
2021-12-16 16:04:11 -0600

    uncommented imports

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -11,5 +11,5 @@ from api.v1.views.states import * # will not be checked
 from api.v1.views.cities import * # will not be checked
 from api.v1.views.amenities import * # will not be checked
 from api.v1.views.users import * # will not be checked
-#from api.v1.views.places import * # will not be checked
-#from api.v1.views.places_reviews import * # will not be checked
+from api.v1.views.places import * # will not be checked
+from api.v1.views.places_reviews import * # will not be checked
