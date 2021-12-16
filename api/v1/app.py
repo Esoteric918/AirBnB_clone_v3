a6a1ece3bcdecff7c87d6cd0d82dff818fc6c737
Hope-Parnell <parnell.hope.c@gmail.com>
2021-12-16 11:06:48 -0600

    created app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""sets up Flask app"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+# Start Flask app
+app = Flask()
+
+
+@app.teardown_appcontext
+def teardown(context):
+    """reloads storage after each request"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    # run the app host
+    # defaults:
+    #   host 0.0.0.0
+    #   port 5000
+    host = getenv("HBNB_API_HOST", '0.0.0.0')
+    port = getenv("HBNB_API_PORT", '5000')
+    app.run(host=host, port=port, threaded=True)
