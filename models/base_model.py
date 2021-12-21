d6b18d9d61cfc454f204ea7891ee91d896133dce
Esoteric918 <3411@holbertonschool.com>
2021-12-21 08:30:19 -0600

    task 15 Security improvements

--- models/base_model.py
+++ models/base_model.py
@@ -11,6 +11,8 @@ from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
 
+from models.user import password
+
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
 if models.storage_t == "db":
@@ -68,6 +70,8 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if not password:
+            new_dict.pop('password', None)
         return new_dict
 
     def delete(self):
